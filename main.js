e766232985a882d84abe5884ae478ca09fdd86d6
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2017-07-18 16:17:36 -0500

    exercise completer

---
+++ main.js
@@ -0,0 +1,27 @@
+let colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+let reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+
+let hohohoElement = document.getElementById("reindeer");
+
+
+	for(let i = 0; i < reindeer.length && reindeer.length <= colors.length; i++){
+		
+
+	let currentReindeer = reindeer[i];
+	let currentColor = colors[i];
+
+		let hohohoElement = document.getElementById("reindeer");
+
+		hohohoElement.innerHTML += `${currentColor} ${currentReindeer} <br>`;
+		
+
+		}
+
+
+
+
+
+
+
+
+
